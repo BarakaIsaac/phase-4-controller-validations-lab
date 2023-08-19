eb5cde00eb4c7c82597296109fb65ae38f50ed8e
BarakaIsaac <isaac.mwandau2@gmail.com>
2023-08-19 08:48:12 +0300

    Completed Assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :category, inclusion: {in:  ['Fiction', 'Non-Fiction'], message: "%{value} is not a valid category" }
+  validates :content, length: { minimum: 100}
 end
